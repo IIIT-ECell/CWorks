685ae17309b62ee5ac9b6a69b912378cac30845e
zubairabid <zubairabid1999+github@gmail.com>
2019-01-14 20:33:00 +0530

    Added a User model with different User roles Changes made only to the backend. Frontend work left.

--- users/models.py
+++ users/models.py
@@ -1,17 +1,124 @@
+'''
+Model description for Student and Company users. The users are an extension of
+Django's default AbstractUser so as to further use built in features available
+through the framework.
+Generic workflow used: https://simpleisbetterthancomplex.com/tutorial/2018/01/18/how-to-implement-multiple-user-types-with-django.html
+'''
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-# from ratings.models import Ratings
 
 
 class CustomUser(AbstractUser):
-    name = models.CharField(blank=True, max_length=255)
-    age = models.IntegerField(default=0)
-    school = models.CharField(blank=True, max_length=255)
-    location = models.CharField(blank=True, max_length=255)
-    bio = models.CharField(blank=True, max_length=1023)
+    '''
+    Model that handles authentication.
+    Setup defaults applicable to both Students and Company.
+    '''
 
-    average_rating_given = models.DecimalField(default=0.0, max_digits=2, decimal_places=2)
-    average_rating_received = models.DecimalField(default=0.0, max_digits=2, decimal_places=2)
+    # Set user type at initialisation, somehow
+    USER_TYPE_CHOICES = (
+        (1, 'Student'),
+        (2, 'Company'),
+        (3, 'Admin'),
+    )
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=1)
+
+    name = models.CharField(blank=False, max_length=255, default='')
+    phone_number = models.CharField(blank=False, max_length=10, default='')
+
+    def __str__(self):
+        return self.email
+
+
+class Student(models.Model):
+    '''
+    Definition of the student, containing
+    Personal details:
+        Name, DOB, ID, Year of study (to be updated yearly), *Permanent City
+        of Residence, Phone, Email, Nationality, Gender
+    Others:
+        Skills
+        Projects
+    Available to all:
+        Name, *Email, *Age, Skills, *Projects, Year of Study
+    Available to Company accounts to which Applications have been made
+        Name, Email, Phone, Age, Skills, Projects, Year of Study, Permanent
+        City of Residence, *Nationality
+    To be added:
+        Rating/Comments by Company
+    '''
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    # Defining choices for Gender and Year of Study
+    # TODO: Consider separating out?
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+    NOT_SAY = 'N'
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+        (NOT_SAY, 'Prefer not to say'),
+    )
+
+    Y1 = '1'
+    Y2 = '2'
+    Y3 = '3'
+    Y4 = '4'
+    Y5 = '5'
+
+    YEAR_OF_STUDY_CHOICES = (
+        (Y1, 'First Year Undergrad'),
+        (Y2, 'Second Year Undergrad'),
+        (Y3, 'Third Year Undergrad'),
+        (Y4, 'Fourth Year Undergrad'),
+        (Y5, 'Postgrads (5th year DD, PG+))'),
+    )
+
+
+    # name = models.CharField(blank=False, max_length=255)
+    # TODO Store DOB
+    student_id = models.CharField(blank=False, max_length=10)
+    # phone_number = models.CharField(blank=False, max_length=10)
+
+    gender = models.CharField(
+        blank=False,
+        max_length=2,
+        choices=GENDER_CHOICES,
+    )
+    year_of_study = models.CharField(
+        blank=False,
+        max_length=1,
+        choices=YEAR_OF_STUDY_CHOICES,
+    )
+
+    permanent_city_res = models.CharField(blank=True, max_length=100)
+    nationality = models.CharField(blank=True, max_length=100)
+
+    def __str__(self):
+        return self.email
+
+
+class Company(models.Model):
+    '''
+    Definition of the company, containing
+        Company name
+        Company ID
+        About Company
+        Email
+        Phone Number
+    '''
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    # name = models.CharField(blank=False, max_length=255)
+    company_id = models.CharField(blank=False, max_length=255)
+    about = models.CharField(blank=False, max_length=10000)
+    # phone_number = models.CharField(blank=False, max_length=10)
+
+    additional_poc = models.CharField(blank=True, max_length=512)
 
     def __str__(self):
         return self.email
--- users/views.py
+++ users/views.py
@@ -1,17 +1,29 @@
 from rest_framework import viewsets, permissions, generics
 
-from .models import CustomUser
-from .serializers import UserSerializer
+from .models import Student, Company
+from .serializers import StudentSerializer, CompanySerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
     permission_classes = [permissions.AllowAny, ]
-    serializer_class = UserSerializer
+    serializer_class = StudentSerializer
 
-class UserAPI(generics.RetrieveAPIView):
+
+class StudentAPI(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated, ]
-    serializer_class = UserSerializer
+    serializer_class = StudentSerializer
 
     def get_object(self):
         return self.request.user
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    permission_classes = [permissions.AllowAny, ]
+    serializer_class = CompanySerializer
+
+
+class CompanyAPI(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+    serializer_class = CompanySerializer
