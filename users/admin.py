ac193c432e61a7e9c3cdb32e91e9db3f77c19f2a
Pulkit gera <pulkithifi@yahoo.com>
2019-02-01 02:09:11 +0530

    name updated

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Job, Company, Student
+from .models import Job, Company, Student, CustomUser
 
 admin.site.register(Job)
 admin.site.register(Company)
 admin.site.register(Student)
+admin.site.register(CustomUser)
