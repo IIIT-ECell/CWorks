8ecc2c9cf390d7e12d37a53afb5d9804f711feb2
zubairabid <zubairabid1999+github@gmail.com>
2019-02-23 14:43:58 +0530

    Boilerplate for jobs

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from .views import GoogleLogin
 
 urlpatterns = [
     path('users/', include('users.urls')),
+    path('jobs/', include('jobs.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('rest-auth/google/', GoogleLogin.as_view(), name='google_login')
---
+++ jobs/urls.py
@@ -0,0 +1,2 @@
+from django.urls import include, path
+from rest_framework import routers
