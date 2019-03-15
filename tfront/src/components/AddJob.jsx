f6e8242ff747e70d936d9b865453eeb625e4597e
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2019-03-15 20:37:24 +0530

    added job addition

--- tfront/src/components/AddJob.jsx
+++ tfront/src/components/AddJob.jsx
@@ -9,14 +9,43 @@ class AddJob extends Component{
     constructor() {
         super();
         this.formData = {};
+        this.handleInput = this.handleInput.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleInput(event) {
         event.preventDefault();
+        console.log(this.formData);
         this.formData[event.target.id] = event.target.value;
     }
 
-    handleSubmit(){
-
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(this.formData);
+        axios({
+            method: 'POST',
+            url: "http://localhost:8000/api/users/jobs/",
+            data:{
+                job_name: this.formData.job_name,
+                description: this.formData.description,
+                skill: this.formData.skill,
+                job_start_date: this.formData.job_start_date,
+                job_duration: this.formData.job_duration,
+                stipend: this.formData.stipend,
+                language: this.formData.language,
+                category: this.formData.category,
+                company_id: sessionStorage["pk"],
+            },
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then((response)=>{
+            this.props.history.push("/jobs");
+        })
+        .catch(response => {
+			console.log("Error");
+			console.error(response);
+		})
     }
 
     render() {
@@ -29,74 +58,72 @@ class AddJob extends Component{
         }
 
         return(
-            <form onSubmit={this.handleInput} className="form-signin">
-                <div className="form-group ">
+            <form onSubmit={this.handleSubmit} className="form-signin">
+                <div className="form-group">
                     <label className="col-sm-2 control-label ">
                         Job name
                     </label>
                     <div className="col-sm-10">
-                        <input name="job_name" className="form-control" type="text"/>
+                        <input name="job_name" id="job_name" className="form-control" type="text" onChange={this.handleInput}/>
                     </div>
                 </div>
-                <div className="form-group ">
+                <div className="form-group">
                     <label className="col-sm-2 control-label ">
                         Description
                     </label>
                     <div className="col-sm-10">
+                        <input name="description" id="description" className="form-control" type="text" onChange={this.handleInput}/>
                     </div>
                 </div>
-                <div className="form-group ">
+                <div className="form-group">
                     <label className="col-sm-2 control-label ">
                     Skill
                     </label>
                     <div className="col-sm-10">
-                        <input name="skill" className="form-control" type="text"/>
+                        <input name="skill" id="skill" className="form-control" type="text" onChange={this.handleInput}/>
                     </div>
                 </div>
-                <div className="form-group ">
+                <div className="form-group">
                     <label className="col-sm-2 control-label ">
                     Job start date
                     </label>
                     <div className="col-sm-10">
-                        <input name="job_start_date" className="form-control" type="date"/>
+                        <input name="job_start_date" id="job_start_date" className="form-control" type="date" onChange={this.handleInput}/>
                     </div>
                 </div>
-                <div className="form-group ">
+                <div className="form-group">
                     <label className="col-sm-2 control-label ">
                     Job duration
                     </label>
                     <div className="col-sm-10">
-                        <input name="job_duration" className="form-control" type="number"/>
+                        <input name="job_duration" id="job_duration" className="form-control" type="number" onChange={this.handleInput}/>
                     </div>
                 </div>
-                <div className="form-group ">
-                    <label className="col-sm-2 control-label ">
+                <div className="form-group">
+                    <label className="col-sm-2 control-label">
                     Stipend
                     </label>
                     <div className="col-sm-10">
-                        <input name="stipend" className="form-control" type="number"/>
+                        <input name="stipend" id="stipend" className="form-control" type="number" onChange={this.handleInput}/>
                     </div>
                 </div>
-                <div className="form-group ">
-                    <label className="col-sm-2 control-label ">
+                <div className="form-group">
+                    <label className="col-sm-2 control-label">
                     Language
                     </label>
                     <div className="col-sm-10">
-                        <input name="language" className="form-control" type="text"/>
+                        <input name="language" id="language" className="form-control" type="text" onChange={this.handleInput}/>
                     </div>
                 </div>
-                <div className="form-group ">
+                <div className="form-group">
                     <label className="col-sm-2 control-label ">
                     Category
                     </label>
                     <div className="col-sm-10">
-                        <input name="category" className="form-control" type="text"/>
+                        <input name="category" id="category" className="form-control" type="text" onChange={this.handleInput}/>
                     </div>
                 </div>
-                <div className="form-actions">
-                    <button className="btn btn-primary" type="submit" title="Make a POST request on the Job List resource">POST</button>
-                </div>
-
+                <button className="btn btn-primary" id="loginbutton" type="submit">Add Job</button>
             </form>
         )
     }
