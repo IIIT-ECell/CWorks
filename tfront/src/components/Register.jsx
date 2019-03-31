544f00e077879e4a9e38602a16ddf428c6b721e5
yoogottamk <yoogottamk@outlook.com>
2019-03-31 12:13:15 +0530

    Some small visual changes

--- tfront/src/components/Register.jsx
+++ tfront/src/components/Register.jsx
@@ -11,13 +11,13 @@ class Register extends Component {
     }
 
     return (
-      <div className="col-md-6">
+      <div className="trial">
         <h1 className="text-center">Register as:</h1>
 
         <div className="mb-5"></div>
 
         <div className="row">
-            <div className="col-md-6 col-sm-12"><Link to='/register/student' className='btn btn-primary btn-block'>Student</Link></div>
+            <div className="col-md-6 col-sm-12 mb-2"><Link to='/register/student' className='btn btn-primary btn-block'>Student</Link></div>
             <div className="col-md-6 col-sm-12"><Link to='/register/company' className='btn btn-primary btn-block'>Company</Link></div>
         </div>
       </div>
